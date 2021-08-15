aa4623f56b1616618a6ae2c93ef788067fc8e51a
Carlos Córdova <ccordova.zosma@gmail.com>
2021-08-14 22:06:07 -0500

    navbar - hero and newsletter

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'black': '#282828',
+        'main': '#FF4C60'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
